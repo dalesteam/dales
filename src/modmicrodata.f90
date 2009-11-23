9978199c82f58a602b592736185552e8c8267f79
steef <sjboing@gmail.com>
2009-11-23 12:40:47 +0100

    Initial commit for ice microphysics

--- src/modmicrodata.f90
+++ src/modmicrodata.f90
@@ -33,7 +33,7 @@
   integer, parameter :: imicro_drizzle = 1
   integer, parameter :: imicro_bulk    = 2
   integer, parameter :: imicro_bin     = 3
-  integer, parameter :: imicro_smplice = 5
+  integer, parameter :: imicro_sice    = 5
   integer, parameter :: imicro_user    = 10
   logical :: l_sb        = .true. , &!< SB scheme (.true.) / KK00 scheme (.false.)   (in namelist NAMMICROPHYSICS)
              l_sedc      = .true. , & !<  cloud droplet sedimentation flag             (in namelist NAMMICROPHYSICS)
