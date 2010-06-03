14e5cb76715c46510a6fe8eb076364b3e9ea0655
Steef <sjboing@gmail.com>
2010-06-03 12:04:25 +0200

    L_graupel inititated

--- src/modmicrophysics.f90
+++ src/modmicrophysics.f90
@@ -44,7 +44,7 @@ contains
     implicit none
     integer :: ierr
     namelist/NAMMICROPHYSICS/ &
-    imicro,l_sb,l_rain,l_sedc,l_mur_cst,l_berry,mur_cst, &     ! OG
+    imicro,l_sb,l_rain,l_sedc,l_mur_cst,l_berry,l_graupel,mur_cst, &     ! OG
     Nc_0, sig_g, sig_gr                                ! SdeR
 
     if(myid==0)then
@@ -64,7 +64,8 @@ contains
     call MPI_BCAST(l_rain,   1, MPI_LOGICAL ,0,comm3d,ierr)
     call MPI_BCAST(l_sedc,   1, MPI_LOGICAL ,0,comm3d,ierr)
     call MPI_BCAST(l_mur_cst,1, MPI_INTEGER ,0,comm3d,ierr)
-    call MPI_BCAST(l_berry,   1, MPI_REAL    ,0,comm3d,ierr)
+    call MPI_BCAST(l_berry,   1, MPI_LOGICAL    ,0,comm3d,ierr)
+    call MPI_BCAST(l_graupel,   1, MPI_LOGICAL    ,0,comm3d,ierr)
     call MPI_BCAST(mur_cst,  1, MPI_REAL    ,0,comm3d,ierr)
     call MPI_BCAST(Nc_0  ,   1, MPI_LOGICAL ,0,comm3d,ierr)
     call MPI_BCAST(sig_g,    1, MPI_INTEGER ,0,comm3d,ierr)
