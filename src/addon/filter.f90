7fd906c9c50fdcb633fa753041de0cf9a37597e3
Chiel <chielvanheerwaarden@gmail.com>
2010-06-24 14:22:14 +0200

    Improved filter to avoid excess communication

--- src/addon/filter.f90
+++ src/addon/filter.f90
@@ -1,24 +1,32 @@
-subroutine filter(v2f, ndx)
+subroutine filter(v2f, ndx, ghostx, ghosty)
   use modglobal, only : i1,ih,i2,j1,jh,j2
   ! top hat filter
   ! for now, only filter in horizontal
   implicit none
 
-  integer, intent(in)    :: ndx
-  real,    intent(inout) :: v2f(2-ih:i1+ih,2-jh:j1+jh)
-  real                   :: v2fin(2-ih:i1+ih,2-jh:j1+jh)
+  integer, intent(in)    :: ndx, ghostx, ghosty
+  real,    intent(inout) :: v2f(2-ghostx:i1+ghostx,2-ghosty:j1+ghosty)
+  real                   :: v2fin(2-ghostx:i1+ghostx,2-ghosty:j1+ghosty)
   integer                :: i,j,m,n
   real                   :: weight
+  integer                :: starti, startj, endi, endj
 
   v2fin(:,:) = v2f(:,:)
-  v2f(:,:) = 0.
+  v2f(:,:)   = 0.
   
   weight   = ndx**2.
 
-  ! Filter width is even
   if(mod(ndx,2) == 0) then
-    do j = 2,j1
-      do i = 2,i1
+    ! Filter width is even
+    starti = 2  - ghostx + ndx / 2
+    startj = 2  - ghosty + ndx / 2
+    endi   = i1 + ghostx - ndx / 2
+    endj   = j1 + ghosty - ndx / 2
+
+    if((starti > 2 ) .or. (startj > 2)) stop 'ERROR: Filter larger than array'
+
+    do j = startj, endj
+      do i = starti, endi
         do m = - ndx / 2, ndx / 2
           do n = - ndx / 2, ndx / 2
             ! Check if we are on the left edge or right edge
@@ -44,10 +52,18 @@ subroutine filter(v2f, ndx)
         v2f(i,j) = v2f(i,j) / weight
       end do
     end do
+
   else
     ! Filter width is odd
-    do j = 2,j1
-      do i = 2,i1
+    starti = 2  - ghostx + (ndx - 1) / 2
+    startj = 2  - ghosty + (ndx - 1) / 2
+    endi   = i1 + ghostx - (ndx - 1) / 2
+    endj   = j1 + ghosty - (ndx - 1) / 2
+    
+    if((starti > 2 ) .or. (startj > 2)) stop 'ERROR: Filter larger than array'
+
+    do j = startj, endj
+      do i = starti, endi
         do m = - (ndx - 1) / 2, (ndx - 1) / 2
           do n = - (ndx - 1) / 2, (ndx - 1) / 2
              v2f(i,j) = v2f(i,j) + v2fin(i+m,j+n)
