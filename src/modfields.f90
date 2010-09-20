796bf66e04c9e9fb6f26465b54b25a8e56599aba
Steef <sjboing@gmail.com>
2010-09-20 11:27:50 +0200

    Added divergence to modfields

--- src/modfields.f90
+++ src/modfields.f90
@@ -137,6 +137,7 @@ save
   real, allocatable :: dummy3(:,:,:)
   real, allocatable :: dummy4(:,:,:)
   real, allocatable :: dummy5(:,:,:)
+  real, allocatable :: dummy6(:,:,:)
 
 contains
 !> Allocate and initialize the prognostic variables
@@ -175,6 +176,7 @@ subroutine initfields
     allocate(dummy3(2-ih:i1+ih,2-jh:j1+jh,k1))
     allocate(dummy4(2-ih:i1+ih,2-jh:j1+jh,k1))
     allocate(dummy5(2-ih:i1+ih,2-jh:j1+jh,k1))
+    allocate(dummy6(2-ih:i1+ih,2-jh:j1+jh,k1))
 
     ! Allocation of base state variables
     allocate(rhobf(k1))
@@ -264,7 +266,7 @@ subroutine initfields
     dthvdz=0.
     SW_up_TOA=0.;SW_dn_TOA=0.;LW_up_TOA=0.;LW_dn_TOA=0.
     qvsl=0.;qvsi=0.;esl=0.
-    dummy1=0.;dummy2=0.;dummy3=0.;dummy4=0.;dummy5=0.
+    dummy1=0.;dummy2=0.;dummy3=0.;dummy4=0.;dummy5=0.;dummy6=0.
 
   end subroutine initfields
 
@@ -283,7 +285,7 @@ subroutine initfields
     deallocate(thlpcar)
     deallocate(SW_up_TOA,SW_dn_TOA,LW_up_TOA,LW_dn_TOA)
     deallocate(qvsl,qvsi,esl)
-    deallocate(dummy1,dummy2,dummy3,dummy4,dummy5)
+    deallocate(dummy1,dummy2,dummy3,dummy4,dummy5,dummy6)
 
    end subroutine exitfields
 
