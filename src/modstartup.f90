390b0cf928cdc154b6ed6f18f7d3db7d18b341f9
Thijs Heus <thijsheus@gmail.com>
2016-07-14 10:18:17 -0400

    WCTime: The second time call needs to be tend, not t start

--- src/modstartup.f90
+++ src/modstartup.f90
@@ -1020,7 +1020,7 @@ contains
     if (tstart < 0) then
       call mpi_get_time(tstart)
     else
-      call mpi_get_time(tstart)
+      call mpi_get_time(tend)
       if (tend-tstart>=wctime) then
         write (*,*) wctime, "NO WALL CLOCK TIME LEFT"
         timeleft=0
