6f81225ae672868727135778a6de93f3b0bd88f2
gvdoord <g.vandenOord@gmail.com>
2016-08-23 17:20:05 +0200

    Made input path 256 bytes

--- src/daleslib.f90
+++ src/daleslib.f90
@@ -100,7 +100,7 @@ module daleslib
 
             implicit none
 
-            character(len=512), intent(in) :: path
+            character(len=256), intent(in) :: path
 
             !----------------------------------------------------------------
             !     1      READ NAMELISTS,INITIALISE GRID, CONSTANTS AND FIELDS
