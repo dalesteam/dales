c7b2df152c2f9b20e239221620df755ba8626c9c
Fredrik Jansson <fjansson@abo.fi>
2020-01-10 15:26:22 +0100

    fixes for the git version string

--- src/git-version.cmake
+++ src/git-version.cmake
@@ -1,6 +1,8 @@
 # Get version from git, at build time
-# Inspiration from CMake Cookbook by Bast and di Remigio
+# Inspiration from CMake Cookbook by Bast and di Remigio,
 # https://github.com/dev-cafe/cmake-cookbook/blob/master/chapter-06/recipe-07/cxx-example/CMakeLists.txt
+# and
+# https://crascit.com/2017/04/18/generated-sources-in-cmake-builds/
 
 set (GIT_VERSION "unknown")
 find_package(Git QUIET)
@@ -10,12 +12,14 @@ if(GIT_FOUND)
     OUTPUT_VARIABLE GIT_VERSION
     ERROR_QUIET
     OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
+    ) 
 endif()
 
-message(STATUS "GIT_VERSION = ${GIT_VERSION} ${CMAKE_CURRENT_BINARY_DIR}/modversion.f90")
+message(STATUS "GIT_VERSION = ${GIT_VERSION} ${TARGET_DIR}/modversion.f90")
 
 configure_file(
   modversion.f90.in
-  ${CMAKE_CURRENT_BINARY_DIR}/modversion.f90
+  ${TARGET_DIR}/modversion.f90
   )
+
+message(STATUS "done")
