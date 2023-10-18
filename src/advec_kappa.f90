fcc23bd8e32d0cd800bbddf1d6ba4dc2e72dc4de
Lucas Esclapez <lesclapez@esciencecenter.nl>
2023-10-18 16:30:56 +0200

    First OpenACC pass on advec_kappa scheme.

--- src/advec_kappa.f90
+++ src/advec_kappa.f90
@@ -55,35 +55,56 @@ subroutine advecc_kappa(a_in,a_out)
   implicit none
   real(field_r), dimension(2-ih:i1+ih,2-jh:j1+jh,k1), intent(in) :: a_in
   real(field_r), dimension(2-ih:i1+ih,2-jh:j1+jh,k1), intent(inout) :: a_out
-  
-  real      d1,d2,cf
-  real :: d1m, d2m, d1p, cfm, cfp, work
-  integer   i,j,k,k_low,k_high
 
-  ! find the lowest and highest k level with a non-zero value in a_in 
+  real :: d1, d2, cf
+  real :: d1m, d2m, d1p, cfm, cfp, work
+  integer :: i, j, k, k_low, k_high, kbeg, kend
+  integer :: nonZeroFound
+
+  ! find the lowest and highest k level with a non-zero value in a_in
   k_low = -1
+  nonZeroFound = 0
   do k=1,k1
-     if (any(a_in(:,:,k).ne.0.)) then
+     !$acc parallel loop collapse(2) default(present) reduction(max:nonZeroFound)
+     do j=2,j1
+        do i=2,i1
+           if (a_in(i,j,k).ne.0.) then
+              nonZeroFound = 1
+           endif
+        enddo
+     enddo
+     if (nonZeroFound) then
         k_low = k
         exit
      endif
   enddo
+
   if (k_low == -1) then
      ! a_in == zero
      return
   endif
 
+  nonZeroFound = 0
   do k=k1,1,-1
-     if (any(a_in(:,:,k).ne.0.)) then
+     !$acc parallel loop collapse(2) default(present) reduction(max:nonZeroFound)
+     do j=2,j1
+        do i=2,i1
+           if ((a_in(i,k,k).ne.0.)) then
+              nonZeroFound = 1
+           endif
+        enddo
+     enddo
+     if (nonZeroFound) then
         k_high = k
         exit
      endif
   enddo
 
+  ! vertical advection from layer 1 to 2, special case. k=2
   if (k_low <= 3) then
-     ! vertical advection from layer 1 to 2, special case. k=2
+     !$acc parallel loop collapse(2) default(present) async(1)
      do j=2,j1
-        do i=2,i1 ! YES
+        do i=2,i1
            d1m = 0
            d2m = rhobf(2) * a_in(i,j,2) - rhobf(1) * a_in(i,j,1)
            cfm = rhobf(1) * a_in(i,j,1)
@@ -111,10 +132,14 @@ subroutine advecc_kappa(a_in,a_out)
      end do
   end if
 
-  !do k=1,kmax
-  do k= max(k_low-1,1), min(k_high+2, kmax) ! loop accesses k-2, k-1, k, k+1
-     do j=2,j1
-        do i=2,i2 ! YES
+  ! loop accesses k-2, k-1, k, k+1
+  kbeg = max(k_low-1,1)
+  kend = min(k_high+2, kmax)
+
+  !$acc parallel loop collapse(3) default(present) async(1)
+  do k = kbeg, kend
+     do j = 2, j1
+        do i = 2, i2
            d2m = a_in(i  ,j,k)-a_in(i-1,j,k)
            d1m = a_in(i-1,j,k)-a_in(i-2,j,k)
            d1p = a_in(i  ,j,k)-a_in(i+1,j,k)
@@ -136,15 +161,18 @@ subroutine advecc_kappa(a_in,a_out)
                 (sign(0.5, d1) + sign(0.5, d2))
 
            work = work * u0(i,j,k) * dxi
+           !$acc atomic update
            a_out(i-1,j,k) = a_out(i-1,j,k) - work
+           !$acc atomic update
            a_out(i,j,k)   = a_out(i,j,k)   + work
         end do
      end do
-     !  end do
+  end do
 
-     !  do k=1,kmax
-     do j=2,j2
-        do i=2,i1 ! YES
+  !$acc parallel loop collapse(3) default(present) async(2)
+  do k = kbeg, kend
+     do j = 2, j2
+        do i = 2, i1
            d1m = a_in(i,j-1,k)-a_in(i,j-2,k)
            d1p = a_in(i,j  ,k)-a_in(i,j+1,k)
            d2m = a_in(i,j  ,k)-a_in(i,j-1,k)
@@ -166,48 +194,52 @@ subroutine advecc_kappa(a_in,a_out)
                 (sign(0.5, d1) + sign(0.5, d2))
 
            work = work * v0(i,j,k) * dyi
+           !$acc atomic update
            a_out(i,j-1,k) = a_out(i,j-1,k) - work
+           !$acc atomic update
            a_out(i,j,k)   = a_out(i,j,k)   + work
         end do
      end do
-     !  end do
-
-     !  do k=3,kmax
-     if (k >= 3) then
-        do j=2,j1
-           do i=2,i1 ! YES
-              d1m = rhobf(k-1) * a_in(i,j,k-1) - rhobf(k-2) * a_in(i,j,k-2)
-              d2m = rhobf(k)   * a_in(i,j,k  ) - rhobf(k-1) * a_in(i,j,k-1)
-              d1p = rhobf(k)   * a_in(i,j,k  ) - rhobf(k+1) * a_in(i,j,k+1)
-              cfm = rhobf(k-1) * a_in(i,j,k-1)
-              cfp = rhobf(k)   * a_in(i,j,k  )
-
-              if (w0(i,j,k) > 0) then
-                 d1 = d1m
-                 d2 = d2m
-                 cf = cfm
-              else
-                 d1 = d1p
-                 d2 = -d2m
-                 cf = cfp
-              end if
-
-              work = cf + &
-                   min(abs(d1), abs(d2), abs((d1/6.0) + (d2/3.0))) * &
-                   (sign(0.5, d1) + sign(0.5, d2))
-
-              work = work * w0(i,j,k)
-              a_out(i,j,k-1) = a_out(i,j,k-1) - (1./(rhobf(k-1)*dzf(k-1)))*work
-              a_out(i,j,k)   = a_out(i,j,k)   + (1./(rhobf(k)  *dzf(k)  ))*work
-           end do
-        end do
-     end if
   end do
+
+  !$acc parallel loop collapse(3) default(present) async(3)
+  do k = 3, kend
+     do j = 2, j1
+        do i = 2, i1
+           d1m = rhobf(k-1) * a_in(i,j,k-1) - rhobf(k-2) * a_in(i,j,k-2)
+           d2m = rhobf(k)   * a_in(i,j,k  ) - rhobf(k-1) * a_in(i,j,k-1)
+           d1p = rhobf(k)   * a_in(i,j,k  ) - rhobf(k+1) * a_in(i,j,k+1)
+           cfm = rhobf(k-1) * a_in(i,j,k-1)
+           cfp = rhobf(k)   * a_in(i,j,k  )
+
+           if (w0(i,j,k) > 0) then
+              d1 = d1m
+              d2 = d2m
+              cf = cfm
+           else
+              d1 = d1p
+              d2 = -d2m
+              cf = cfp
+           end if
+
+           work = cf + &
+                min(abs(d1), abs(d2), abs((d1/6.0) + (d2/3.0))) * &
+                (sign(0.5, d1) + sign(0.5, d2))
+
+           work = work * w0(i,j,k)
+           !$acc atomic update
+           a_out(i,j,k-1) = a_out(i,j,k-1) - (1./(rhobf(k-1)*dzf(k-1)))*work
+           !$acc atomic update
+           a_out(i,j,k)   = a_out(i,j,k)   + (1./(rhobf(k)  *dzf(k)  ))*work
+        end do
+     end do
+  end do
+
 end subroutine advecc_kappa
 
 subroutine  halflev_kappa(a_in,a_out)
 
-  use modglobal, only : i1,ih,j1,jh,k1
+    use modglobal, only : i1,ih,j1,jh,k1
     use modfields, only : w0, rhobf, rhobh
     implicit none
     real(field_r), dimension(2-ih:i1+ih,2-jh:j1+jh,k1), intent(in) :: a_in
@@ -215,7 +247,7 @@ subroutine  halflev_kappa(a_in,a_out)
     real(field_r)      d1,d2,cf
     integer   i,j,k
 
-
+    !$acc parallel loop collapse(3) private(d1,d2,cf) default(present) async(1)
     do k=3,k1
       do j=2,j1
         do i=2,i1
@@ -233,6 +265,7 @@ subroutine  halflev_kappa(a_in,a_out)
       end do
     end do
 
+    !$acc parallel loop collapse(2) private(d1,d2,cf) default(present) async(2)
     do j=2,j1
       do i=2,i1
         if (w0(i,j,2)>=0) then
@@ -263,7 +296,5 @@ subroutine  halflev_kappa(a_in,a_out)
     phir  = max(0.,min(2.*ri,min(1./3.+2./3.*ri,2.)))
     rlim  = 0.5*phir*d1
     end function rlim
-
-
 
 end module advec_kappa
