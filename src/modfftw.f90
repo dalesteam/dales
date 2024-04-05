6dde03e4aafba82c5d2221006b80d5a46aaa2463
Fredrik Jansson <fjansson@abo.fi>
2024-04-05 12:27:19 +0300

    modfftw: fix zeroing unused part of workspace in last tile in row or column
    fix for the case when the data *is* evenly divisible over the tiles.
    Found by Frans Liqui Lung

--- src/modfftw.f90
+++ src/modfftw.f90
@@ -704,8 +704,8 @@ contains
       call transpose_a2(p210, p201)
 
       ! zero the unused part, avoinds SIGFPE from the FFT (Debug mode)
-      if (myidx == nprocx-1) p201(:,konx_last+1:, :) = 0
-      if (myidy == nprocy-1) p201(:,:,iony_last+1:) = 0
+      if (myidx == nprocx-1 .and. konx_last < konx) p201(:,konx_last+1:, :) = 0
+      if (myidy == nprocy-1 .and. iony_last < iony) p201(:,:,iony_last+1:) = 0
       ! p201(jtot,konx,iony)
 
       call fftw_execute_r2r_if(plany, p201_flat, p201_flat)
