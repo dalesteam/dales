f86cbb823543cc66ad000d006311e044d5085814
Caspar Jungbacker <74240267+CasparJungbacker@users.noreply.github.com>
2024-04-25 20:15:08 +0200

    Tests: first prototype

--- tests/test_bomex.py
+++ tests/test_bomex.py
@@ -1,4 +1,15 @@
+import subprocess
+import os
+
 import pytest
 
-def test_run():
-    pass
+@pytest.mark.parametrize(
+    "nprocs", [1, 2, 3, 4]
+)
+def test_run(nprocs):
+    """Run DALES, check if it runs succesfully (exit code equal to zero).
+    The environment variable DALES has to point to the DALES executable to run.
+    """
+    dales = os.environ["DALES"]
+    result = subprocess.run(["mpirun", "-np", str(nprocs), dales, "namoptions.001"], cwd="cases/bomex")
+    assert result.returncode == 0
