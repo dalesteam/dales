18ccb0aef142551d4b16aecdc07ddc9d967a8866
Caspar Jungbacker <74240267+CasparJungbacker@users.noreply.github.com>
2024-05-01 11:01:11 +0200

    Tests: test for multiple domain configs

--- tests/test_run.py
+++ tests/test_run.py
@@ -4,22 +4,52 @@ import os
 import pytest
 import f90nml
 
+@pytest.mark.parametrize("kmax", [64, 61])
 @pytest.mark.parametrize(
-    "nprocs", [1, 2]
+    "nprocx,nprocy,itot,jtot", 
+    [
+        (1, 3, 32, 48),
+        (2, 2, 32, 32),
+        (1, 1, 31, 31)
+    ]
 )
-def test_run(nprocs, tmp_case):
-    """Run DALES, check if it runs succesfully.
+def test_domains(
+    tmp_case,
+    nprocx,
+    nprocy,
+    itot,
+    jtot,
+    kmax
+):
+    """Run DALES for interesting domains, check if it runs succesfully.
     The environment variable DALES has to point to the DALES executable to run.
     """
     dales = os.environ["DALES"]
 
+    # Patch namoptions
+    patch = {}
+
+    patch["DOMAIN"] = {
+        "itot": itot,
+        "jtot": jtot,
+        "kmax": kmax
+    }
+
+    patch["RUN"] = {
+        "runtime": 100,
+        "nprocx": nprocx,
+        "nprocy": nprocy
+    }
+
     namopts = f90nml.read(tmp_case / "namoptions.001")
-    namopts["RUN"]["runtime"] = 100
+    namopts.patch(patch)
     namopts.write(tmp_case / "namoptions.001", force=True)
+
+    nprocs = nprocx * nprocy
 
     # Run DALES as a subprocess, capturing its output
     result = subprocess.run(
-        ["mpirun", "-np", str(nprocs), dales, "namoptions.001"],
+        ["mpirun", "-oversubscribe", "-np", str(nprocs), dales, "namoptions.001"],
         cwd=tmp_case,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
