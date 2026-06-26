3a227338325766c9d38c7ff4667c7742b45c9aa0
Loris Lucido <loris.lucido@bull.com>
2026-06-26 16:56:14 +0200

    WIP: modthermodynamics omp gpu 4

--- src/modslabaverage.f90
+++ src/modslabaverage.f90
@@ -32,11 +32,10 @@ module modslabaverage
   character(len=*), parameter :: modname = 'modslabaverage'
 
   public :: slabavg
-  public :: slabavg_r8_gpu
-  public :: slabavg_r8_gpu2
+  public :: slabavg_gpu
 
   interface slabavg
-    module procedure slabavg_r4
+     module procedure slabavg_r4
     module procedure slabavg_r8
     module procedure slabavg_r4_masked
     module procedure slabavg_r8_masked
@@ -117,86 +116,7 @@ contains
   !!           \note For arrays with no ghost cells, pass nh=0!
   !! \param avg Slab averaged profile.
   !! \param local Only compute average on local MPI domain.
-  subroutine slabavg_r8_gpu2(field, nh, avg, local)
-
-    real(real64), intent(in)  :: field(:,:,:)
-    integer,      intent(in)  :: nh
-    real(real64), intent(out) :: avg(:)
-
-    logical, optional, intent(in) :: local
-
-    integer      :: i, j, k
-    integer      :: is, ie, js, je, ks, ke
-    logical      :: do_global
-    real(real64) :: fld_sum, norm_fac
-
-    is = lbound(field, dim=1) + nh
-    ie = ubound(field, dim=1) - nh
-    js = lbound(field, dim=2) + nh
-    je = ubound(field, dim=2) - nh
-    ks = lbound(field, dim=3)
-    ke = ubound(field, dim=3)
-
-    if (present(local)) then
-      do_global = .not. local
-    else
-      do_global = .true.
-    end if
-
-    if (do_global) then
-      norm_fac = 1.0_real64 / ijtot
-    else
-      norm_fac = 1.0_real64 / (imax * jmax)
-    end if
-    !$omp target update to(field)
-
-    avg=0
-    do k = ks, ke
-      fld_sum = 0
-      do j = js, je
-        do i = is, ie
-          fld_sum = fld_sum + field(i,j,k)
-        end do
-      end do
-      avg(k) = fld_sum * norm_fac
-    end do
-#if 0
-    print *, ">>> avg1", sum(avg)
-    avg=0
-
-    !$acc parallel loop gang default(present)
-    !!$omp target teams loop defaultmap(present:aggregate)&
-    !!$omp defaultmap(present:allocatable)
-    do k = ks, ke
-      fld_sum = 0
-      !$acc loop vector collapse(2) reduction(+: fld_sum)
-      !$omp target teams loop defaultmap(present:aggregate) defaultmap(present:allocatable)&
-      !$omp reduction(+:fld_sum) collapse(2)
-      !!$omp loop reduction(+:fld_sum) collapse(2)
-      do j = js, je
-        do i = is, ie
-          fld_sum = fld_sum + field(i,j,k)
-        end do
-      end do
-      !$omp taskwait
-      avg(k) = fld_sum * norm_fac
-    end do
-
-    print *, ">>> avg2", sum(avg)
-#endif
-
-    !!$omp target update to(avg)
-    if (do_global) then
-      !$acc host_data use_device(avg)
-      !!$omp target data use_device_addr(avg)
-      call mpi_allreduce(mpi_in_place, avg, ke, mpi_real8, mpi_sum, &
-                         comm3d, mpierr)
-      !$acc end host_data
-      !!$omp end target data
-    end if
-
-  end subroutine slabavg_r8_gpu2
-  subroutine slabavg_r8_gpu(field, nh, avg, local)
+  subroutine slabavg_gpu(field, nh, avg, local)
 
     real(real64), contiguous, intent(in)  :: field(:,:,:)
     integer,      intent(in)  :: nh
@@ -228,37 +148,27 @@ contains
       norm_fac = 1.0_real64 / (imax * jmax)
     end if
 
-    ! do k = ks, ke
-    !   fld_sum = 0
-    !   do j = js, je
-    !     do i = is, ie
-    !       fld_sum = fld_sum + field(i,j,k)
-    !     end do
-    !   end do
-    !   avg(k) = fld_sum * norm_fac
-    ! end do
-    ! print *, ">>> avg1", sum(avg)
-
     !$acc parallel loop gang default(present)
-    !!$omp target teams loop defaultmap(present:aggregate)&
-    !!$omp defaultmap(present:allocatable)
+    !!$omp target defaultmap(present:allocatable)
+    !!$omp target teams distribute
     do k = ks, ke
       fld_sum = 0
       !$acc loop vector collapse(2) reduction(+: fld_sum)
-      !$omp target teams loop defaultmap(present:aggregate) defaultmap(present:allocatable)&
-      !$omp reduction(+:fld_sum) collapse(2)
-      !!$omp loop reduction(+:fld_sum) collapse(2)
+      !!$omp parallel do reduction(+:fld_sum) collapse(2)
+      !!$omp target defaultmap(present:allocatable)
+      !!$omp target teams distribute parallel do reduction(+:fld_sum) collapse(2) defaultmap(present:allocatable)
       do j = js, je
         do i = is, ie
           fld_sum = fld_sum + field(i,j,k)
         end do
       end do
+      !!$omp end target
       avg(k) = fld_sum * norm_fac
     end do
+    !!$omp end target
 
-    ! print *, ">>> avg2", sum(avg)
+    !!$omp target update from(avg)
 
-    !!$omp target update to(avg)
     if (do_global) then
       !$acc host_data use_device(avg)
       !!$omp target data use_device_addr(avg)
@@ -267,20 +177,27 @@ contains
       !$acc end host_data
       !!$omp end target data
     end if
+    !$omp target update to(avg)
 
-  end subroutine slabavg_r8_gpu
-  subroutine slabavg_r8(field, nh, avg, local)
+  end subroutine slabavg_gpu
+
+  subroutine slabavg_r8(field, nh, avg, local,toto)
 
     real(real64), intent(in)  :: field(:,:,:)
     integer,      intent(in)  :: nh
     real(real64), intent(out) :: avg(:)
 
-    logical, optional, intent(in) :: local
+    logical, optional, intent(in) :: local,toto
 
     integer      :: i, j, k
     integer      :: is, ie, js, je, ks, ke
-    logical      :: do_global
+    logical      :: do_global,tata
     real(real64) :: fld_sum, norm_fac
+    if(present(toto)) then
+       tata=.true.
+    else
+       tata=.false.
+    endif
 
     is = lbound(field, dim=1) + nh
     ie = ubound(field, dim=1) - nh
@@ -315,6 +232,9 @@ contains
       end do
       avg(k) = fld_sum * norm_fac
     end do
+
+    if(tata)&
+    print *, ">>> avg cpu", sum(avg), minval(avg), maxval(avg)
 
     if (do_global) then
       !$acc host_data use_device(avg)
