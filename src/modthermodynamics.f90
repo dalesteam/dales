93bf60d4ecbe1f93d0055dead03ca7ae4ea5323d
Loris Lucido <loris.lucido@bull.com>
2026-07-02 08:37:39 +0200

    disable slabavg gpu reduction

--- src/modthermodynamics.f90
+++ src/modthermodynamics.f90
@@ -172,7 +172,7 @@ contains
 
     !$omp target update to(u0, v0, thl0, qt0, ql0)
     !$omp target update to(presf, presh, exnf, exnh, ql0h)
-    !$omp target update to(zf, zh, dzf, dzhi, dthldz, dqtdz)
+    !$omp target update to(zf, zh, dzf, dzh, dzhi, dthldz, dqtdz)
 
     if (timee < 0.01) then
       call diagfld
@@ -577,7 +577,6 @@ contains
 
     ! 2.1 Use first guess of theta, then recalculate theta
 
-    !$omp target update to(dzf,dzh)
     call fromztop
 
     !$acc parallel loop gang(static:1) default(present) async(1)
