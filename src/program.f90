37989f5afe2369d95c9a5c4bb988a2b153f39718
Loris Lucido <loris.lucido@bull.com>
2026-07-04 09:40:46 +0200

    modnudge omp gpu

--- src/program.f90
+++ src/program.f90
@@ -249,7 +249,6 @@ program DALES
         call samptend(tend_force)
 
         call lstend !large scale forcings
-        host_is_updated=.false.; call update_host
         call samptend(tend_ls)
         call microphysics
         call samptend(tend_micro)
@@ -259,6 +258,7 @@ program DALES
     !   3.6   EXECUTE ADD ONS
     !------------------------------------------------------
         call nudge
+        host_is_updated=.false.; call update_host
         call nudgeboundary
         call testbednudge
         if (simid == turid) call spraying
