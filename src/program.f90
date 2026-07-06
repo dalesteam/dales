07682a18270ea1049f814e519fa27a78400d8408
Loris Lucido <loris.lucido@bull.com>
2026-07-06 11:49:32 +0200

    WIP: modgenstat omp gpu (with mpi on cpu)

--- src/program.f90
+++ src/program.f90
@@ -312,11 +312,11 @@ program DALES
     !------------------------------------------------------
         if (simid == statid) then
           call twostep
-          host_is_updated=.false.; call update_host
           !call coldedge
           call checksim
           call timestat  !Timestat must preceed all other timeseries that could write in the same netCDF file (unless stated otherwise
           call genstat  !Genstat must preceed all other statistics that could write in the same netCDF file (unless stated otherwise
+          host_is_updated=.false.; call update_host
           call write_profiles
           call radstat
           call lsmstat
