8cb11cf584f8fedce31edc483a086dfe6f8be46e
Loris Lucido <loris.lucido@bull.com>
2026-07-09 14:55:56 +0200

    modradfield, modbudget, modvarbudget: uncomment omp gpu update

--- src/program.f90
+++ src/program.f90
@@ -331,7 +331,6 @@ program DALES
           !call projection
           call cloudfield
           call fielddump
-          host_is_updated=.false.; call update_host
           call radfield
           !call particles
 
@@ -340,6 +339,7 @@ program DALES
           call msebudg2
           !call stressbudgetstat
           call heterostats
+          host_is_updated=.false.; call update_host
 
           call testwctime
           call writerestartfiles
