02eda4b70b1ace46352a01dc29a2144a6ab288f1
Loris Lucido <loris.lucido@bull.com>
2026-07-09 18:22:38 +0200

    rrtmgp: set KERNEL_MODE to accel and update rrtmgp with more amdflang workarounds

--- cmake/dales_add_rrtmgp.cmake
+++ cmake/dales_add_rrtmgp.cmake
@@ -4,7 +4,7 @@ set( BUILD_C_HEADERS OFF )
 set( RTE_ENABLE_SP ${ENABLE_FP32_RAD} )
 set( KERNEL_MODE "default" CACHE STRING "Set kernel mode for RTE" )
 
-if( ${ENABLE_ACC} )
+if( ${ENABLE_ACC} OR ${ENABLE_OMP_GPU} )
     set( KERNEL_MODE "accel" )
 endif()
 
@@ -13,17 +13,19 @@ ecbuild_info( "Fetching RTE-RRTMGP" )
 FetchContent_Declare(
     rrtmgp
     GIT_REPOSITORY "https://github.com/dindon-sournois/rte-rrtmgp.git"
-    GIT_TAG 66d65b314c292c220980b98e5c90820664bba8a3 # v1.9.2 with OpenMP and AMDFlang fix
+    GIT_TAG 6b5d8f46aae1058d4bf15de87253b4e3eaf53ac5 # v1.9.2 with OpenMP and AMDFlang fix
 )
 
 FetchContent_MakeAvailable( rrtmgp )
 
 # For some reason, RRTMGP doesn't pick up the OpenACC compiler flag,
 # so we set it here manually
-target_compile_options( rrtmgp PUBLIC ${OpenACC_Fortran_FLAGS} )
-target_compile_options( rrtmgpkernels PUBLIC ${OpenACC_Fortran_FLAGS} )
-target_compile_options( rte PUBLIC ${OpenACC_Fortran_FLAGS} )
-target_compile_options( rtekernels PUBLIC ${OpenACC_Fortran_FLAGS} )
+if( ${ENABLE_ACC} )
+  target_compile_options( rrtmgp PUBLIC ${OpenACC_Fortran_FLAGS} )
+  target_compile_options( rrtmgpkernels PUBLIC ${OpenACC_Fortran_FLAGS} )
+  target_compile_options( rte PUBLIC ${OpenACC_Fortran_FLAGS} )
+  target_compile_options( rtekernels PUBLIC ${OpenACC_Fortran_FLAGS} )
+endif()
 
 # remove SIMD from pure routine
 if( ${CMAKE_Fortran_COMPILER_ID} MATCHES Cray )
