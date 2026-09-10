45b8abce412c59e88b30a391fbbd77dcfda9f66e
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2026-09-10 13:03:02 +0000

    Nudging: add missing broadcast

--- src/modnudge.f90
+++ src/modnudge.f90
@@ -104,6 +104,7 @@ contains
     call D_MPI_BCAST(lwnudge, 1, 0, comm3d, mpierr)
     call D_MPI_BCAST(lthlnudge, 1, 0, comm3d, mpierr)
     call D_MPI_BCAST(lqtnudge, 1, 0, comm3d, mpierr)
+    call D_MPI_BCAST(lsvnudge, 1, 0, comm3d, mpierr)
     call D_MPI_BCAST(tnudgefac, 1, 0, comm3d, mpierr)
     call D_MPI_BCAST(ltthlnudge, 1, 0, comm3d, mpierr)
 
